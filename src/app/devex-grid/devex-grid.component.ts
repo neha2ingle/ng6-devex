392af11bf0add1686bac7224639749f5a07fba0c
neha ingle <neha2ingle@gmail.com>
2019-10-15 22:34:23 +0530

    json binding

--- src/app/devex-grid/devex-grid.component.ts
+++ src/app/devex-grid/devex-grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService, Customer } from '../data.service';
+import DevExpress from 'devextreme/bundles/dx.all';
 
 @Component({
   selector: 'ng6-devex-grid',
@@ -9,11 +10,40 @@ import { DataService, Customer } from '../data.service';
 export class DevexGridComponent implements OnInit {
 
   customers: Customer[];
+  gridColumns: Array<DevExpress.ui.dxDataGridColumn | string>
 
-    constructor(service: DataService) {
-        this.customers =  service.getCustomers();
-    }
+  constructor(private service: DataService) {
+
+  }
   ngOnInit() {
+    this.customers = this.service.getCustomers();
+    this.gridColumns = [
+      {
+        caption: 'Company Name',
+        dataField: 'CompanyName',
+        width: 200
+      },
+      {
+        caption: 'My City',
+        dataField: 'City',
+        width: 200
+      },
+      {
+        caption: 'My State',
+        dataField: 'State',
+        width: 200
+      },
+      {
+        caption: 'My Phone',
+        dataField: 'Phone',
+        width: 200
+      },
+      {
+        caption: 'My Fax',
+        dataField: 'Fax',
+        width: 200
+      }
+    ];
   }
 
 }
